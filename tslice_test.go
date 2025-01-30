1bd6f7fd70900c094935007b529c161ab90ffb8b
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-01-30 14:51:22 +0000

    TEST: cover Filter

--- tslice_test.go
+++ tslice_test.go
@@ -7,13 +7,24 @@ import (
 )
 
 func TestFilter(t *testing.T) {
-	dataArr := []int{0, 1, -1, 2}
+	tests := []struct {
+		name         string
+		targets      []int
+		expectLength int
+	}{
+		{"Test success filter", []int{0, 1, -1, 2}, 3},
+		{"Test 0 length case", []int{}, 0},
+	}
 
-	filtered := tslice.Filter(dataArr, func(data int) bool {
-		return data >= 0
-	})
+	for _, it := range tests {
+		t.Run(it.name, func(t *testing.T) {
+			filtered := tslice.Filter(it.targets, func(data int) bool {
+				return data >= 0
+			})
 
-	if len(filtered) != 3 {
-		t.Errorf("Unmatched filter: %v", filtered)
+			if len(filtered) != it.expectLength {
+				t.Errorf("Unmatched filter: %v", filtered)
+			}
+		})
 	}
 }
